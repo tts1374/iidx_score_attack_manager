3b28526ce169d71a1d3cf28babb0bb844051a9be
2ten. <tts1374@gmail.com>
2026-02-27 17:57:13 +0900

    chore(i18n): add locale files for ja/en/ko

--- packages/web-app/src/i18n/index.ts
+++ packages/web-app/src/i18n/index.ts
@@ -1,10 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
+import en from './locales/en.json';
+import ja from './locales/ja.json';
+import ko from './locales/ko.json';
+
 const resources = {
-  ja: { translation: {} },
-  en: { translation: {} },
-  ko: { translation: {} },
+  ja: { translation: ja },
+  en: { translation: en },
+  ko: { translation: ko },
 } as const;
 
 void i18n.use(initReactI18next).init({
